711ba24ecd8dfa6b01c7487558e0057e91de6eb2
Conrado Cañas <conradocanas@gmail.com>
2020-08-26 17:04:53 -0300

    Cambios en card

--- src/components/Dolar.jsx
+++ src/components/Dolar.jsx
@@ -1,53 +1,44 @@
 import React, { Fragment } from 'react';
-import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, Card, ListGroup, CardColumns } from 'react-bootstrap';
+import "./Dolar.css";
 
 const Dolar = ({oficial, blue, turista}) => {
   
     return ( 
         <Fragment>
             <Container>
-            <Row>
-                <Col>
-                    <Row>
-                        <Card 
-                            className="dolarOficial"
-                            style={{ width: '18rem' }}
-                        >
-                        <Card.Header>{oficial.nombre}</Card.Header>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item><Row><Col><p>Compra</p>{oficial.compra}</Col><Col><p>Ventanpm</p>{oficial.venta}</Col></Row></ListGroup.Item>
-                            <ListGroup.Item>{oficial.variacion}</ListGroup.Item>
+            <CardColumns>
+                <Card 
+                    bg="success"
+                    className="dolarOficial"
+                    text="white"
+                >
+                    <Card.Header> <h5>{oficial.nombre}</h5></Card.Header>
+                    <Card.Body 
+                        bg="success"
+                    >
+                        <ListGroup variant="flush" className="text-center">
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col >
+                                        <h6>Compra</h6>
+                                        <h3>{oficial.compra} </h3>
+                                    </Col>
+                                    <Col>
+                                        <h6>Venta</h6>
+                                        <h3>{oficial.venta}</h3>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
                         </ListGroup>
-                        </Card>
-                    </Row>
-                </Col>
-                <Col>
-                    <Row>
-                        <Card 
-                            className="dolarBlue"
-                            style={{ width: '18rem' }}>
-                        <Card.Header>{blue.nombre}</Card.Header>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item><Row><Col><p>Compra</p>{blue.compra}</Col><Col><p>Venta</p>{blue.venta}</Col></Row></ListGroup.Item>
-                            <ListGroup.Item>{blue.variacion}</ListGroup.Item>
-                        </ListGroup>
-                        </Card>
-                    </Row>
-                </Col>
-                <Col>
-                    <Row>
-                        <Card
-                            className="dolarTurista"
-                            style={{ width: '18rem' }}>
-                        <Card.Header>{turista.nombre}</Card.Header>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item><Row><Col><p>Compra</p>{turista.compra}</Col><Col><p>Venta</p>{turista.venta}</Col></Row></ListGroup.Item>
-                            <ListGroup.Item>{turista.variacion}</ListGroup.Item>
-                        </ListGroup>
-                        </Card>
-                    </Row>
-                </Col>
-            </Row>
+                    </Card.Body>
+                    <Card.Footer className="text-center">
+                        <span>Variación: {oficial.variacion}</span>
+                    </Card.Footer>
+                </Card>
+            </CardColumns>
+
+
             <Row>
                 <Col>Noticias</Col>
             </Row>
