2a211baddccf76c73b8385d74b7377cb426da14a
Conrado Cañas <conradocanas@gmail.com>
2020-08-27 17:23:44 -0300

    News updated

--- src/components/Noticias.jsx
+++ src/components/Noticias.jsx
@@ -1,25 +1,23 @@
 import React, {Fragment,useState, useEffect} from 'react'
 import { Container, CardDeck, Card } from 'react-bootstrap';
+import "./noticias.css"
+
 
 const Noticias = ({noticias}) => {
-
-    
     return ( 
         <Fragment>
             <Container>
-                <CardDeck>
+                <CardDeck className="noticias">
                 {noticias.slice(0, 4).map(noticia => (
                      <Card>
                         <Card.Img key={noticia.url} variant="top" src={noticia.urlToImage} />
                         <Card.Header key={noticia.url}>
-                         <h6>{noticia.title}</h6>
+                         <h6  className="titulo-noticia">{noticia.title}</h6>
                         </Card.Header>
                      <Card.Body>
-                         <p key={noticia.url}>{noticia.title}</p>
+                         <p className="contenido-noticia" key={noticia.url}>{noticia.title}</p>
+                         <p className="fuente-noticia" key={noticia.url}> Fuente: {noticia.author}</p>
                      </Card.Body>
-                     <Card.Footer className="text-center">
-                         <span key={noticia.url}> Fuente: {noticia.author}</span>
-                     </Card.Footer>
                  </Card>
                 ))}
                 </CardDeck>
