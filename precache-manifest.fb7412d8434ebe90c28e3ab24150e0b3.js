1f1f4fd539f9df77226bc4932b3c34c1a80720ba
NazgulFS <59717278+NazgulFS@users.noreply.github.com>
2020-09-28 14:35:10 -0300

    Updates

---
+++ precache-manifest.fb7412d8434ebe90c28e3ab24150e0b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da499528a2b069441e5fe49a557bc023",
+    "url": "/NazgulFS/Dolar/index.html"
+  },
+  {
+    "revision": "1a2c512804fdb1208dd8",
+    "url": "/NazgulFS/Dolar/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ab393a31a3746535014d",
+    "url": "/NazgulFS/Dolar/static/css/main.eadf2f12.chunk.css"
+  },
+  {
+    "revision": "1a2c512804fdb1208dd8",
+    "url": "/NazgulFS/Dolar/static/js/2.5dbd70b3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/NazgulFS/Dolar/static/js/2.5dbd70b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab393a31a3746535014d",
+    "url": "/NazgulFS/Dolar/static/js/main.71e63b18.chunk.js"
+  },
+  {
+    "revision": "6ac7b51eaf46ac22e62d",
+    "url": "/NazgulFS/Dolar/static/js/runtime-main.97ca1d6f.js"
+  }
+]);
